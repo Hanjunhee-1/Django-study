3c1f53bc72b6e0567100ecef1c1119663e9a8f97
Hanjunhee-1 <hanjuni857@naver.com>
2023-04-05 13:34:13 +0900

    뷰 내용 추가

--- django_tutorial/polls/views.py
+++ django_tutorial/polls/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse("Hello, world. You're at the polls index.")
+
+    # latest_question_list = Question.objects.order_by('-publish_date')[:5]
+    # output = '<br>'.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+
+    # latest_question_list = Question.objects.order_by('-publish_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+
+    latest_question_list = Question.objects.order_by('-publish_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse('You\'re looking at question %s' % question_id)
